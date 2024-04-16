3c0d5e00fbeaac8130468b45ce8d89db15c0352d
이승규 <artdark12@gmail.com>
2024-04-16 18:56:45 +0900

    어노테이션 추가

--- src/main/java/com/example/carrot_market/user/controller/UserController.java
+++ src/main/java/com/example/carrot_market/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.carrot_market.user.controller;
 
 import com.example.carrot_market.core.BaseResponseEntity;
+import com.example.carrot_market.core.aop.ExeTimer;
 import com.example.carrot_market.user.domain.User;
 import com.example.carrot_market.user.domain.UserAggregate;
 import com.example.carrot_market.user.dto.request.SignInResponseDto;
@@ -9,10 +10,7 @@ import com.example.carrot_market.user.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -22,6 +20,13 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @ExeTimer
+    @GetMapping("/aop_test")
+    public String dbAccess() throws InterruptedException {
+        Thread.sleep(1000);
+        return "db";
     }
 
     @PostMapping("/sign_up")
