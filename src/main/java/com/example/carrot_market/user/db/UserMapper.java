6ac4dde1fdf44b8d2c5bdd2797093019dd50c7f0
Kim Sehyun <kkshyun56@gmail.com>
2024-04-21 10:09:25 +0900

    [feat]User unRegister

--- src/main/java/com/example/carrot_market/user/db/UserMapper.java
+++ src/main/java/com/example/carrot_market/user/db/UserMapper.java
@@ -11,4 +11,5 @@ public interface UserMapper {
     Optional<User> selectUserByPhone(String phone);
     void insertUser(User user);
     void updateUser(User user);
+    void unRegister(User user);
 }
