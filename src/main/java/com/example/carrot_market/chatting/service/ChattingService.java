7e8077e8536bd286ae0f41cd74b0efff145d6ff7
dltmdrb1996 <65164183+dltmdrb1996@users.noreply.github.com>
2024-04-24 23:03:50 +0900

    Feat 채팅목록 변경 (#64)
    feat 상품올리기 비동기식에서 동기식으로 변경

--- src/main/java/com/example/carrot_market/chatting/service/ChattingService.java
+++ src/main/java/com/example/carrot_market/chatting/service/ChattingService.java
@@ -3,6 +3,7 @@ package com.example.carrot_market.chatting.service;
 
 import com.example.carrot_market.chatting.domain.Chat;
 import com.example.carrot_market.chatting.domain.ChatRoom;
+import com.example.carrot_market.chatting.domain.ChatRoomAggregate;
 import com.example.carrot_market.chatting.dto.CreateChatDto;
 import com.example.carrot_market.chatting.dto.CreateChatRoomRequestDto;
 import com.example.carrot_market.chatting.dto.UpdateChatRoomRequestDto;
@@ -20,7 +21,7 @@ public interface ChattingService {
     void exitChattingRoom(int roomId, int userId);
 
     // GET
-    List<ChatRoom> getChatRoomListByUserId(int userId);
+    List<ChatRoomAggregate> getChatRoomListByUserId(int userId);
     ChatRoom getChatRoom(int roomId);
     List<Chat> getChatListByRoomId(int roomId);
 
