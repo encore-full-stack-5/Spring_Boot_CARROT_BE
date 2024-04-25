365048fef83a1789e56fc263b8a96652fbbab3bf
ParkMiram <125454927+ParkMiram@users.noreply.github.com>
2024-04-25 18:00:48 +0900

    [feat] getDetailBoard add (#73)

--- src/main/java/com/example/carrot_market/board/controller/BoardController.java
+++ src/main/java/com/example/carrot_market/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.example.carrot_market.board.controller;
 
 import com.example.carrot_market.board.domain.model.Board;
 import com.example.carrot_market.board.dto.AddBoardRequestDto;
+import com.example.carrot_market.board.dto.getDetailBoardResultDto;
 import com.example.carrot_market.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +26,7 @@ public class BoardController {
     }
 
     // 사용자가 작성한 커뮤니티 글목록 조회
-    @GetMapping("/{id}")
+    @GetMapping("/user/{id}")
     public List<Board> getBoardListByUserId(
             @PathVariable("id") int userId
     ) {
@@ -38,5 +39,13 @@ public class BoardController {
             @PathVariable("id") int areaId
     ) {
         return boardService.getBoardListByAreaId(areaId);
+    }
+
+    // 선택한 커뮤니티의 정보 조회
+    @GetMapping("/board/{id}")
+    public getDetailBoardResultDto getDetailBoard(
+            @PathVariable("id") int boardId
+    ) {
+        return boardService.getDetailBoard(boardId);
     }
 }
